f5dbb0033a5c73fe4845ceb243fd3b8730456e28
Oren Shalmy <orenshalmy@gmail.com>
2025-02-10 19:51:19 +0200

    fixed mediainfo

--- static/js/mediainfo.js
+++ static/js/mediainfo.js
@@ -1,8 +1,7 @@
 function updateOverlayText(jsonData) {
-    const overlayText = document.getElementById('overlayText');
+  const overlayText = document.getElementById('overlayText');
   const duration = jsonData.streams[0].duration;
   const bitRate = jsonData.streams[0].bit_rate;
-  const kbps = bitRate / 1024;
   const fps = parseFloat(jsonData.streams[0].r_frame_rate).toFixed(2);
   const formatName = jsonData.format.format_name;
   const codecName = jsonData.streams[0].codec_name;
@@ -17,7 +16,7 @@ function updateOverlayText(jsonData) {
     <p>Duration: ${duration} seconds</p>
     <p>Resolution: ${width}x${height}</p>
     <p>Frame Rate: ${fps} fps</p>
-    <p>Bit Rate: ${kbps} kb/s</p>
+    <p>Bit Rate: ${(parseInt(bitRate) / 1024).toFixed(2)} kb/s</p>
     <p>Profile: ${profile}</p>
     <p>Level: ${level}</p>
     <p>Pixel Format: ${pixFormat}</p>
